c6fa9646010e9c9790a9eb9ab7cff30d16dafeff
Prasanth-1011 <112630396+Prasanth-1011@users.noreply.github.com>
2025-10-11 16:37:54 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://web-stack-djk5.onrender.com/api';
+const API_URL = 'https://web-stack-x71t.onrender.com/api';
 
 // Theme Management
 const currentTheme = localStorage.getItem('theme') || 'ice';
@@ -146,4 +146,5 @@ if (registerForm) {
             console.error('Registration error:', error);
         }
     });
+
 }
